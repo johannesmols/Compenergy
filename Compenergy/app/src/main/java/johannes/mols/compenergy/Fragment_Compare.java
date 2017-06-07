2c6ca51c9dc6d15a22aa2f0e523a3bd644484546
Johannes Mols <johannes.mols@googlemail.com>
2017-06-07 13:20:15 +0200

    Added animated gradient background for the compare fragment, colors are still awful though

--- Compenergy/app/src/main/java/johannes/mols/compenergy/Fragment_Compare.java
+++ Compenergy/app/src/main/java/johannes/mols/compenergy/Fragment_Compare.java
@@ -4,7 +4,7 @@
 
 package johannes.mols.compenergy;
 
-import android.graphics.drawable.GradientDrawable;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -14,17 +14,34 @@ import android.view.ViewGroup;
 
 public class Fragment_Compare extends Fragment {
 
+    private AnimationDrawable animationDrawable;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_compare_layout, container, false);
 
-        View rootLayout = view.findViewById(R.id.fragment_compare_root_layout);
-        GradientDrawable gd = new GradientDrawable(GradientDrawable.Orientation.TOP_BOTTOM, new int[] {0xFFFFEE58, 0xFFBB6689});
-        gd.setCornerRadius(0f);
-        //rootLayout.setBackground(gd);
+        animationDrawable = (AnimationDrawable) view.getBackground();
+        animationDrawable.setEnterFadeDuration(5000);
+        animationDrawable.setExitFadeDuration(2000);
 
         return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if(animationDrawable != null && !animationDrawable.isRunning()) {
+            animationDrawable.start();
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if(animationDrawable != null && animationDrawable.isRunning()) {
+            animationDrawable.stop();
+        }
     }
 
     @Override

00eb2a59791d798ee38adba5f4ff75ce14866ac4
Johannes Mols <johannes.mols@googlemail.com>
2017-06-07 18:12:45 +0200

    Added a shuffle button

--- Compenergy/app/src/main/java/johannes/mols/compenergy/Fragment_Compare.java
+++ Compenergy/app/src/main/java/johannes/mols/compenergy/Fragment_Compare.java
@@ -4,11 +4,14 @@
 
 package johannes.mols.compenergy;
 
+import android.content.pm.ActivityInfo;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -21,16 +24,25 @@ public class Fragment_Compare extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_compare_layout, container, false);
 
+        setHasOptionsMenu(true);
+
         animationDrawable = (AnimationDrawable) view.getBackground();
         animationDrawable.setEnterFadeDuration(5000);
-        animationDrawable.setExitFadeDuration(2000);
+        animationDrawable.setExitFadeDuration(5000);
 
         return view;
     }
 
     @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        getActivity().setTitle(R.string.nav_item_compare);
+    }
+
+    @Override
     public void onResume() {
         super.onResume();
+        getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         if(animationDrawable != null && !animationDrawable.isRunning()) {
             animationDrawable.start();
         }
@@ -39,15 +51,15 @@ public class Fragment_Compare extends Fragment {
     @Override
     public void onPause() {
         super.onPause();
+        getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_FULL_SENSOR);
         if(animationDrawable != null && animationDrawable.isRunning()) {
             animationDrawable.stop();
         }
     }
 
     @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        getActivity().setTitle(R.string.nav_item_compare);
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.fragment_compare_toolbar, menu);
+        super.onCreateOptionsMenu(menu, inflater);
     }
 }

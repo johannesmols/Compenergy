e04a55cb735aac1ff5a850e2069aa7b8b3ebd78f
Johannes Mols <johannes.mols@googlemail.com>
2017-06-29 16:29:51 +0200

    Added basic data display logic to display items in the favorite fragment

--- Compenergy/app/src/main/java/johannes/mols/compenergy/Fragment_Favorites_Data.java
+++ Compenergy/app/src/main/java/johannes/mols/compenergy/Fragment_Favorites_Data.java
@@ -4,24 +4,137 @@
 
 package johannes.mols.compenergy;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ExpandableListView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
 
 public class Fragment_Favorites_Data extends Fragment {
+
+    private Context mContext;
+
+    private ExpandableListView expandableListView;
+    private List<String> categories_list;
+    private HashMap<String, List<Carrier>> carriers_list;
+    private DataExpandableListAdapter adapter;
+    private EditText searchEditText;
+
+    private DatabaseHelper dbHelper;
+
+    private static final int REQUEST_CODE_EDIT = 0x539;
+
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
         getActivity().setTitle(R.string.nav_item_favorites);
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_favorites_layout, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_favorites_layout, container, false);
+        mContext = getContext();
+        expandableListView = (ExpandableListView) view.findViewById(R.id.fragment_favorites_expandable_list_view);
+        dbHelper = new DatabaseHelper(mContext, null, null, 1);
+
+        displayList();
+
+        expandAllGroups();
+
+        searchEditText = (EditText) view.findViewById(R.id.fragment_favorites_search);
+        searchEditText.setOnTouchListener(editSearchOnTouchListener);
+        searchEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                adapter.filterData(s.toString());
+                expandAllGroups();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        return view;
     }
+
+    private void expandAllGroups() {
+        for(int i = 0; i < adapter.getGroupCount(); i++) {
+            expandableListView.expandGroup(i);
+        }
+    }
+
+    private void displayList() {
+        prepareListData();
+
+        adapter = new DataExpandableListAdapter(mContext, categories_list, carriers_list);
+        expandableListView.setAdapter(adapter);
+
+        expandAllGroups();
+    }
+
+    private void prepareListData() {
+        categories_list = new ArrayList<>();
+        carriers_list = new HashMap<>();
+
+        categories_list = dbHelper.getCategoryList();
+
+        for(int i = 0; i < categories_list.size(); i++) {
+            List<Carrier> carrierList = dbHelper.getCarriersWithCategory(categories_list.get(i));
+            carriers_list.put(categories_list.get(i), carrierList);
+        }
+
+        //Sort
+        Collections.sort(categories_list, CustomComparators.ALPHABETICAL_ORDER);
+        CustomComparators.CarrierComparator comparator = new CustomComparators.CarrierComparator();
+        for (List<Carrier> l : carriers_list.values())
+            Collections.sort(l, comparator);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(resultCode == Activity.RESULT_OK) {
+            displayList();
+        }
+    }
+
+    View.OnTouchListener editSearchOnTouchListener = new View.OnTouchListener() {
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            //final int DRAWABLE_LEFT = 0;
+            //final int DRAWABLE_TOP = 1;
+            final int DRAWABLE_RIGHT = 2;
+            //final int DRAWABLE_BOTTOM = 3;
+
+            if(event.getAction() == MotionEvent.ACTION_UP) {
+                if(event.getRawX() >= (searchEditText.getRight() - searchEditText.getCompoundDrawables()[DRAWABLE_RIGHT].getBounds().width())) {
+                    searchEditText.setText("");
+                    return true;
+                }
+            }
+            return false;
+        }
+    };
 }

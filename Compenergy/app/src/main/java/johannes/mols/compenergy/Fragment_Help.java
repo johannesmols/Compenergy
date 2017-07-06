565f8d0fbc2653bb5340ca0bc4ed59ebd2975bdf
Johannes Mols <johannes.mols@googlemail.com>
2017-07-06 18:24:34 +0200

    Added a TextView to the help fragment, content for the help text still needs to be added

--- Compenergy/app/src/main/java/johannes/mols/compenergy/Fragment_Help.java
+++ Compenergy/app/src/main/java/johannes/mols/compenergy/Fragment_Help.java
@@ -4,6 +4,7 @@
 
 package johannes.mols.compenergy;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -12,6 +13,9 @@ import android.view.View;
 import android.view.ViewGroup;
 
 public class Fragment_Help extends Fragment {
+
+    private Context mContext;
+
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
@@ -22,6 +26,10 @@ public class Fragment_Help extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_help_layout, container, false);
+        View view = inflater.inflate(R.layout.fragment_help_layout, container, false);
+
+        mContext = view.getContext();
+
+        return view;
     }
 }

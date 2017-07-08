e7523467b7c48a61b20f38ccbd99dfa8046ea5b7
Johannes Mols <johannes.mols@googlemail.com>
2017-07-08 12:25:18 +0200

    Added an empty activity for the selection of items

---
+++ Compenergy/app/src/main/java/johannes/mols/compenergy/ActSelectItem.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) Johannes Mols 2017.
+ */
+
+package johannes.mols.compenergy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActSelectItem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_select_item_layout);
+    }
+}

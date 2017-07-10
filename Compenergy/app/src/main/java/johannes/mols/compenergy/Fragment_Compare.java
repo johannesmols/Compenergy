1f43e1ead8f1686b86df674cc9334ddcea82b787
Johannes Mols <johannes.mols@googlemail.com>
2017-07-10 20:08:19 +0200

    Fixed a bug where the energy amount display in the compare fragment would not put a space between the number and unit when the amount is lower than 1000

--- Compenergy/app/src/main/java/johannes/mols/compenergy/Fragment_Compare.java
+++ Compenergy/app/src/main/java/johannes/mols/compenergy/Fragment_Compare.java
@@ -154,9 +154,9 @@ public class Fragment_Compare extends Fragment {
             //Energy
             upperItemEnergy.setText(Util.format(item.get_energy()));
             if(item.get_unit().equalsIgnoreCase(getString(R.string.carrier_type_db_capacity)) || item.get_unit().equalsIgnoreCase(getString(R.string.carrier_type_db_consumption))) {
-                upperItemEnergy.append(getString(R.string.watt_lower_case));
+                upperItemEnergy.append(" " + getString(R.string.watt_lower_case));
             } else {
-                upperItemEnergy.append(getString(R.string.joule_lower_case));
+                upperItemEnergy.append(" " + getString(R.string.joule_lower_case));
             }
 
             //Energy unit
@@ -179,9 +179,9 @@ public class Fragment_Compare extends Fragment {
             //Energy
             lowerItemEnergy.setText(Util.format(item.get_energy()));
             if(item.get_unit().equalsIgnoreCase(getString(R.string.carrier_type_db_capacity)) || item.get_unit().equalsIgnoreCase(getString(R.string.carrier_type_db_consumption))) {
-                lowerItemEnergy.append(getString(R.string.watt_lower_case));
+                lowerItemEnergy.append(" " + getString(R.string.watt_lower_case));
             } else {
-                lowerItemEnergy.append(getString(R.string.joule_lower_case));
+                lowerItemEnergy.append(" " + getString(R.string.joule_lower_case));
             }
 
             //Energy unit
